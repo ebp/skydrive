b5690089a3b900d1db6647c6359efbfb95742fda
Kyle Yetter <kyetter@ebackpack.com>
2013-10-25 16:35:21 -0400

    Update Client#upload to return a Skydrive::File object

--- lib/skydrive/client.rb
+++ lib/skydrive/client.rb
@@ -9,16 +9,26 @@ module Skydrive
 
     def initialize access_token
       @access_token = access_token
+      @user_data    = nil
+    end
+
+    def user_data
+      @user_data  ||= me
+    end
+
+    def user_id
+      user_data["id"]
     end
 
     %w( get post put move delete ).each do |method|
       define_method(method.to_sym) do |url, options = {}|
-        query      = { access_token: @access_token.token }.update( options.fetch( :query, {} ) )
-        options    = options.merge( query: query )
-        return_raw = options.delete( :raw )
+        query      = { access_token: @access_token.token }.update(options.fetch(:query, {}))
+        options    = options.merge(query: query)
+        return_raw = options.delete(:raw)
+        overrides  = options.delete(:overrides) || {}
         response   = self.class.send(method, url, options)
 
-        return_raw ? response : filtered_response(response)
+        return_raw ? response : filtered_response(response, overrides)
       end
     end
 
@@ -49,9 +59,10 @@ module Skydrive
       get("/me/skydrive/search", options)
     end
 
-    def upload(remote_path, file_name, content)
+    def upload(remote_path, file_name, content, options = {})
       file_name = file_name.to_s
       content   = content.respond_to?(:read) ? content.read.to_s : content.to_s
+      size      = ( content.bytesize rescue content.size )
       nl        = "\r\n"
       boundary  = 'A300x'
       body      = ''
@@ -61,25 +72,36 @@ module Skydrive
       body << content << nl
       body << '--' << boundary << '--' << nl
 
-      headers = { 'Content-Type' => "multipart/form-data; boundary=#{ boundary }" }
-      post(remote_path, headers: headers, body: body, format: :plain)
+      extra_properties = { 'size' => size, 'from' => user_data }
+      headers          = { 'Content-Type' => "multipart/form-data; boundary=#{ boundary }" }
+
+      options =
+        options.merge(
+          body:        body,
+          headers:     headers.update(options.fetch(:headers, {})),
+          overrides:   extra_properties.update(options.fetch(:overrides, {}))
+        )
+
+      post(remote_path, options)
     end
 
     private
 
     # Filter the response after checking for any errors
-    def filtered_response response
+    def filtered_response response, overrides = {}
       raise Skydrive::Error.new({"code" => "no_response_received", "message" => "Request didn't make through or response not received"}) unless response
       if response.success?
         filtered_response = response.parsed_response
-        if response.response.code == "200"
+        filtered_response.update(overrides) if filtered_response.is_a?(::Hash)
+
+        if response.response.code =~ /^2/
           raise Skydrive::Error.new(filtered_response["error"], response) if filtered_response["error"]
           if filtered_response["data"]
             return Skydrive::Collection.new(self, filtered_response["data"])
           elsif filtered_response["location"]
             return filtered_response
-          elsif filtered_response["id"].match(/^comment\..+/)
-            return Skydrive::Comment.new(self, filtered_response)
+          elsif filtered_response.key?("id") && type = filtered_response["id"].to_s[/^(comment|file)\./, 1]
+            return Skydrive.const_get(type.capitalize).new(self, filtered_response)
           elsif filtered_response.key?("type")
             return "Skydrive::#{filtered_response["type"].capitalize}".constantize.new(self, filtered_response)
           else
